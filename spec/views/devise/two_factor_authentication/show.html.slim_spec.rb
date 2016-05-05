29a32e19c5f5c4eae43b5b729d341fc38d36cf10
Sam Clegg <sclegg@omb.eop.gov>
2016-05-05 11:03:16 -0400

    Remove support for multiple second factors
    This change removes support for OTP via email and multiple
    second factors in general.  SMS is now the only method of OTP
    delivery.
    
    This simplifies the schema and removes the second_factors table.
    
    Why:
    
    OTP via email as second factor is bad idea since email is the
    recovery mechanism for the first factor.
    
    Supporting multiple seconds factors is nice, but second factors
    are not generic enough to simply be driven by a database table.
    Currently we only support SMS as a second factor so we can
    keep the code simple by removing the addition database and
    supporting code.

--- spec/views/devise/two_factor_authentication/show.html.slim_spec.rb
+++ spec/views/devise/two_factor_authentication/show.html.slim_spec.rb
@@ -4,38 +4,22 @@ describe 'devise/two_factor_authentication/show.html.slim' do
   context 'user has unconfirmed_mobile and is two_factor_enabled' do
     it 'only mentions mobile' do
       allow(view).to receive(:current_user).
-        and_return(create(:user, :tfa_confirmed, unconfirmed_mobile: '5005550006'))
+        and_return(create(:user, :signed_up, unconfirmed_mobile: '5005550999'))
 
       render
 
-      expect(rendered).to have_content 'A one-time passcode has been sent to 5005550006.'
+      expect(rendered).to have_content 'A one-time passcode has been sent to +1 (500) 555-0999.'
     end
   end
 
-  context 'user has 2 second factors but no unconfirmed email' do
-    it 'mentions both email and mobile' do
+  context 'user has second factor and no unconfirmed email' do
+    it 'mentions mobile' do
       allow(view).to receive(:current_user).
-        and_return(
-          create(
-            :user, :signed_up, :both_tfa_confirmed, email: 'foo@bar.com', mobile: '5005550006'
-          )
-        )
+        and_return(create(:user, :signed_up, :with_mobile, mobile: '5005550777'))
 
       render
 
-      expect(rendered).
-        to have_content 'A one-time passcode has been sent to foo@bar.com and +1 (500) 555-0006.'
-    end
-  end
-
-  context 'user only has email second factor and no unconfirmed email' do
-    it 'mentions email' do
-      allow(view).to receive(:current_user).
-        and_return(create(:user, :signed_up, email: 'foo@bar.com'))
-
-      render
-
-      expect(rendered).to have_content 'A one-time passcode has been sent to foo@bar.com.'
+      expect(rendered).to have_content 'A one-time passcode has been sent to +1 (500) 555-0777.'
     end
 
     it 'prompts the user to enter an OTP' do
@@ -45,16 +29,6 @@ describe 'devise/two_factor_authentication/show.html.slim' do
 
       expect(rendered).
         to have_content t('devise.two_factor_authentication.header_text')
-    end
-  end
-
-  context 'user only has mobile second factor and no unconfirmed email' do
-    it 'mentions mobile' do
-      allow(view).to receive(:current_user).and_return(create(:user, :signed_up, :with_mobile))
-
-      render
-
-      expect(rendered).to have_content 'A one-time passcode has been sent to +1 (500) 555-0006.'
     end
   end
 end
